d86313d5013e6fb997f2f04a13850dc201bfe7c8
Valerii Mazurenko <Vmazurenko@gmail.com>
2021-11-21 16:12:28 +0200

    hw8p1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,33 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+const galleryItemsList = document.querySelector('.gallery');
+function galleryItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a 
+      class="gallery__item" 
+      href="${original}"
+      >
+      <img 
+      class="gallery__image" 
+      src="${preview}" 
+      alt="${description}"  
+      />
+    </a>`;
+    })
+    .join('');
+}
+galleryItemsList.insertAdjacentHTML('afterbegin', galleryItem(galleryItems));
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: `alt`,
+  captionType: 'attr',
+  captionDelay: 250,
+});
+lightbox();
+
+console.log(galleryItems);
