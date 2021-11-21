0eb09e6be9115836464006bd2ee4a80744f0079a
Valerii Mazurenko <Vmazurenko@gmail.com>
2021-11-21 21:26:19 +0200

    reinstall_node_version

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeUpdate', function (data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify);
+});
+
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
+
+player.setCurrentTime();
